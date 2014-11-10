6d55938bd0552a4494e9e9e9c28df1a8f82f7d56
hakbra <haakbraa@online.no>
2014-11-10 15:29:18 +0100

    Lagt til wms-funksjon

---
+++ SGIS/Photo.cs
@@ -0,0 +1,31 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGIS
+{
+    class Photo
+    {
+        public Photo(System.Drawing.Image i, ScreenManager.SGISEnvelope b)
+        {
+            Pic = i;
+            Bounds = new ScreenManager.SGISEnvelope(b);
+            
+            OgcCompliantGeometryFactory fact = new OgcCompliantGeometryFactory();
+            Geometry = fact.ToGeometry(b);
+        }
+        public ScreenManager.SGISEnvelope Bounds;
+        public System.Drawing.Image Pic;
+        public GeoAPI.Geometries.IGeometry Geometry;
+
+        public void Draw(System.Drawing.Graphics mapGraphics)
+        {
+           Rectangle screenRect = SGIS.App.ScreenManager.MapRealToScreen(Bounds);
+           mapGraphics.DrawImage(Pic, screenRect);
+        }
+    }
+}
